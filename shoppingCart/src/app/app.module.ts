b75737684088fc2705deb3c689029f132699c63f
krishhoney <krish.siva007@gmail.com>
2018-06-03 03:48:27 -0700

    first commit

---
+++ shoppingCart/src/app/app.module.ts
@@ -0,0 +1,29 @@
+//import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2/index';
+
+import { environment } from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  AngularFireModule.initializeApp(environment.firebase),
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ shoppingCart/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBXZxZcKswbDm4pE3YSAjWKfG1oEfPmm6U",
+    authDomain: "shoppingcart-66044.firebaseapp.com",
+    databaseURL: "https://shoppingcart-66044.firebaseio.com",
+    projectId: "shoppingcart-66044",
+    storageBucket: "",
+    messagingSenderId: "288511886388"
+  }
+};
